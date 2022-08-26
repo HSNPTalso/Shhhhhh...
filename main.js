ac59f6c2e42455f42a9a769b2f8229b9b664dc8e
HSNPTalso <112161665+HSNPTalso@users.noreply.github.com>
2022-08-26 17:22:27 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let image = document.querySelector('img');
+
+image.onclick = function() {
+    let src = image.getAttribute('src');
+    if(src === 'images/呼叫.gif') {
+      image.setAttribute('src', 'images/嗷.png');
+    } else {
+      image.setAttribute('src', 'images/呼叫.gif');
+    }
+}
+/*将img元素的引用存放在image变量里*/
+/*将匿名函数赋值给了image变量的点击属性
+获取image变量的src的属性值，存放于src变量里
+如果src为①，将image的src属性设置为②
+提醒;
+不同，则将image的src属性设置为①*/
